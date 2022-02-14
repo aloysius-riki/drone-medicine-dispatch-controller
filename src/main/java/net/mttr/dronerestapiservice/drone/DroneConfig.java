205a5cc43b2e757f97322f519e458f4fba5beaa9
Aloysius Riki <68075271+aloysius-riki@users.noreply.github.com>
2022-02-14 20:08:45 +0300

    Added sample required data that must be preloaded in database

--- src/main/java/net/mttr/dronerestapiservice/drone/DroneConfig.java
+++ src/main/java/net/mttr/dronerestapiservice/drone/DroneConfig.java
@@ -16,21 +16,86 @@ public class DroneConfig {
                    "SerialNumber1",
                    100,
                    10,
-                   "Model1",
-                   "State1",
+                   "Lightweight",
+                   "RETURNING",
                    "Medication1"
            );
            Drone two = new Drone(
                    "SerialNumber2",
-                   200,
-                   20,
-                   "Model2",
-                   "State2",
+                   150,
+                   15,
+                   "Lightweight",
+                   "RETURNING",
                    "Medication2"
+           );
+           Drone three = new Drone(
+                   "SerialNumber1",
+                   180,
+                   20,
+                   "Middleweight",
+                   "RETURNING",
+                   "Medication1"
+           );
+           Drone four = new Drone(
+                   "SerialNumber1",
+                   200,
+                   40,
+                   "Middleweight",
+                   "DELIVERED",
+                   "Medication1"
+           );
+           Drone five = new Drone(
+                   "SerialNumber1",
+                   250,
+                   65,
+                   "Cruiserweight",
+                   "DELIVERING",
+                   "Medication1"
+           );
+           Drone six = new Drone(
+                   "SerialNumber1",
+                   300,
+                   80,
+                   "Cruiserweight",
+                   "DELIVERING",
+                   "Medication1"
+           );
+           Drone seven = new Drone(
+                   "SerialNumber1",
+                   350,
+                   85,
+                   "Cruiserweight",
+                   "LOADED",
+                   "Medication1"
+           );
+           Drone eight = new Drone(
+                   "SerialNumber1",
+                   400,
+                   90,
+                   "Heavyweight",
+                   "LOADING",
+                   "Medication1"
+           );
+           Drone nine = new Drone(
+                   "SerialNumber1",
+                   450,
+                   95,
+                   "Heavyweight",
+                   "IDLE",
+                   "Medication9"
+           );
+           Drone ten = new Drone(
+                   "SerialNumber10",
+                   500,
+                   100,
+                   "Heavyweight",
+                   "IDLE",
+                   "Medication10"
            );
 
            droneRepository.saveAll(
-                   List.of(one, two)
+                   List.of(one, two, three, four, five, six, seven,
+                           eight, nine, ten)
            );
         } ;
     }
