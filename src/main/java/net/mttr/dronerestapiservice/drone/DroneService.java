03b63d0c487b2dedcabfb4a97f015d7811fcc998
Aloysius Riki <68075271+aloysius-riki@users.noreply.github.com>
2022-02-14 17:21:35 +0300

    Implement data access layer for drones package

--- src/main/java/net/mttr/dronerestapiservice/drone/DroneService.java
+++ src/main/java/net/mttr/dronerestapiservice/drone/DroneService.java
@@ -1,20 +1,22 @@
 package net.mttr.dronerestapiservice.drone;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class DroneService {
+
+    private final DroneRepository droneRepository;
+
+    @Autowired
+    public DroneService(DroneRepository droneRepository) {
+        this.droneRepository = droneRepository;
+    }
+
+
     public List<Drone> getDrones() {
-        return List.of(
-                new Drone(
-                        "SerialNumber",
-                        500,
-                        45,
-                        "Model",
-                        "State"
-                )
-        );
+        return droneRepository.findAll();
     }
 }
